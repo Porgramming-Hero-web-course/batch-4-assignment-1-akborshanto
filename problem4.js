f59a5e84f1212da7044791767abc37f7cb045d31
AKBOR SHANTO <akborshanto11111@gmail.com>
2024-11-08 02:20:41 -0800

    problem 4

--- problem4.js
+++ problem4.js
@@ -1,2 +1,22 @@
 "use strict";
-console.log("dsfa");
+function calculateShapeArea(shape) {
+    if (shape.shape === "circle") {
+        // Type guard for Circle
+        return parseFloat((Math.PI * shape.radius ** 2).toFixed(2));
+    }
+    else if (shape.shape === "rectangle") {
+        // Type guard for Rectangle
+        return shape.width * shape.height;
+    }
+    throw new Error("Unsupported shape");
+}
+// Sample Input 1
+const circleArea = calculateShapeArea({ shape: "circle", radius: 5 });
+console.log(circleArea); // Output: 78.54
+// Sample Input 2
+const rectangleArea = calculateShapeArea({
+    shape: "rectangle",
+    width: 4,
+    height: 6,
+});
+console.log(rectangleArea); 
